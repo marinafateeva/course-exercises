fa20531019944533eddbe65f00d2ec20aa9be1dc
Marina <Marina.Fateeva@edu.salpaus.fi>
2024-09-23 17:32:26 +0300

    Added updated task to my home computer

--- Projekti (valinnainen tehtävä)/projekti.js
+++ Projekti (valinnainen tehtävä)/projekti.js
@@ -1,9 +1,10 @@
 let secondField = document.getElementById("tieto").value;
 
-function fieldAlert()
+function fieldAlert() {
 if (secondField.length < 1){
     document.getElementById("button1").style.color = "red";
 }
 else { 
     document.getElementById("result").innerHTML =    
 }
+};
