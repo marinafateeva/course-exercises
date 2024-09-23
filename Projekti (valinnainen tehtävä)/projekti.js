3b6466fe8dcfab4234f6e01095f40f9d496843c3
Marina <Marina.Fateeva@edu.salpaus.fi>
2024-09-23 14:12:13 +0300

    modified optional task

--- Projekti (valinnainen tehtävä)/projekti.js
+++ Projekti (valinnainen tehtävä)/projekti.js
@@ -10,12 +10,17 @@ infoDisplay.innerHTML = '';
 
 if (field2Value.trim() === '') {
     errorMsg.style.display = 'block';
+    document.getElementById('button1').style.backgroundColor='red';
+    document.getElementById('button1').innerHTML='Virhe!!! Et lisännyt tietoa'
     setTimeout(function() {
         errorMsg.style.display = 'none';
+        document.getElementById('button1').style.backgroundColor='black';
+        document.getElementById('button1').innerHTML='Lisää tieto'
         }, 3500);
 } else {
     errorMsg.style.display = 'none';
     //infoDisplay.innerHTML = <p>field1Value</p>,<p>field2Value</p>
+    infoDisplay.style.display = 'block';
     document.getElementById('infoDisplay').innerHTML = field1Value + ': ' + field2Value;
 }
 });
