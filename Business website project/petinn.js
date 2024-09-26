3cd7d2de2a8abd962b0e3fe52fe88931d62d314b
Marina <Marina.Fateeva@edu.salpaus.fi>
2024-09-26 14:07:34 +0300

    modified website project

---
+++ Business website project/petinn.js
@@ -0,0 +1,8 @@
+function copyNumber() {
+    navigator.clipboard.writeText
+        ("+1 (123) 4567 890");
+}
+function copyEmail() {
+    navigator.clipboard.writeText
+        ("example@email.com");
+}
